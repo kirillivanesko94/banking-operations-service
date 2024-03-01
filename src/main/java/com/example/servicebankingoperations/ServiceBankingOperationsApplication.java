2ec38d676c26e83aac56ced3b656d8eefec71bce
kirill_ivanesko <kirill.ivanesko@yandex.ru>
2024-03-01 22:24:34 +0300

    initial commit

---
+++ src/main/java/com/example/servicebankingoperations/ServiceBankingOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicebankingoperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBankingOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBankingOperationsApplication.class, args);
+	}
+
+}
