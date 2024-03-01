a5d74e10c7d347d9307045244a76aef81ef4f2ed
kirill_ivanesko <kirill.ivanesko@yandex.ru>
2024-03-01 22:23:32 +0300

    initial commit

---
+++ src/test/java/com/example/servicebankingoperations/ServiceBankingOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicebankingoperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBankingOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
